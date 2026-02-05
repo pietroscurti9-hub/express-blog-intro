d110ea66eb7ebaae2e710f7759529b6802e0584c
Pietro Scurti <pietroscurti9@gmail.com>
2026-02-05 16:15:38 +0100

    array di oggetti creato

--- app.js
+++ app.js
@@ -4,9 +4,54 @@ const port = 3000
 
 const mieiPost = [
     {
-        
+        id: 1,
+        titolo: "Ciambellone",
+        contenuto: "Cibo",
+        img: "/BlogImg/ciambellone.jpeg",
+        tag: "Dolce"
 
-    }
+    },
+
+    {
+        id: 2,
+        titolo: "Cracker barbabietola",
+        contenuto: "Cibo",
+        img: "/BlogImg/cracker_barbabietola.jpeg",
+        tag: "Salato"
+
+    },
+
+    {
+        id: 3,
+        titolo: "Pane fritto dolce",
+        contenuto: "Cibo",
+        img: "/BlogImg/pane_fritto_dolce.jpeg",
+        tag: "Dolce"
+
+    },
+
+    {
+        id: 4,
+        titolo: "Pasta barbabietola",
+        contenuto: "Cibo",
+        img: "BlogImg/pasta_barbabietola.jpeg",
+        tag: "Salato"
+
+    },
+
+    {
+        id: 5,
+        titolo: "Torta paesana",
+        contenuto: "Cibo",
+        img: "BlogImg\torta_paesana.jpeg",
+        tag: "Dolce"
+
+    },
+
+
+
+
+
 ]
 
 app.get('/', (req, res) => {
